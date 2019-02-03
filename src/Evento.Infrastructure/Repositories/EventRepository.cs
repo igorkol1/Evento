5f4478b07abf4f55f98c52cb166569ebeddd4915
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-02-03 18:18:55 +0100

    Implamentation of event repository

---
+++ src/Evento.Infrastructure/Repositories/EventRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Evento.Core.Domain;
+using Evento.Core.Repositories;
+
+namespace Evento.Infrastructure.Repositories
+{
+    public class EventRepository:IEventRepository
+    {
+        public EventRepository()
+        {
+        }
+
+        public Task AddAsync(Event @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Event>> BrowseAsync(string name = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Event @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Event> GetAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Event @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
